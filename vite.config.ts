282ed9785445610935063acab362afb3bf6d3f26
lusuijie <lusuijie@sogou-inc.com>
2022-04-18 19:06:17 +0800

    vite练习

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { normalizePath } from "vite";
+// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+import path from "path";
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve("./src/variable.scss"));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	// css 相关的配置
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// additionalData 的内容会在每个 scss 文件的开头自动注入
+				additionalData: `@import "${variablePath}";`,
+			},
+		},
+	},
+	plugins: [react()],
+});
